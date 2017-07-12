6b38ac1b3e27c01a39bd40dff69a8f50ffed1012
ToF- <cthibaut@octo.com>
2017-07-12 19:14:14 +0200

    rewrite, starting with Cost

--- Dijkstra.hs
+++ Dijkstra.hs
@@ -1,104 +1,17 @@
 module Dijkstra where
-import Data.PSQueue
-import Data.Map
-import Data.List
-
-type Edge     = (Node, Node, Distance)
-type Neighbor = (Node, Distance)
-type RouteMap = Map Node [Neighbor]
-type Distance = Int
-type NodeCount= Int
-type Cost     = (Distance,NodeCount)
-type Node     = Int
-data Route    = Route Cost (Maybe Node)
-    deriving (Eq,Ord,Show)
-type Itinerary = PSQ Node Route
-
-infinite :: Cost
-infinite = (maxBound,maxBound)
-
-cost :: Route -> Cost
-cost (Route c _) = c
-
-distance :: Route -> Distance
-distance (Route (d,c) _) = d
-
-undefinedRoute :: Route
-undefinedRoute = Route infinite Nothing
-
-via :: Route -> Maybe Node
-via (Route _ n) = n
-
-route :: Cost -> Node -> Route
-route c n = Route c (Just n)
-
-initialRoute :: Route
-initialRoute = Route (0,0) Nothing
-
-itinerary :: [Node] -> Node -> Itinerary
-itinerary ns st = Data.PSQueue.fromList $ Prelude.map initRoute ns
-    where
-    initRoute :: Node -> Binding Node Route
-    initRoute n | n == st   = n :-> initialRoute
-                | otherwise = n :-> Route infinite Nothing
-
-updateRoute :: Cost -> Node -> Route -> Route
-updateRoute c n r | c < cost r = Route c (Just n)
-                  | otherwise  = r
 
 
-updateItinerary :: Itinerary -> [Neighbor] -> Neighbor -> NodeCount -> Itinerary
-updateItinerary i [] _ _ = i
-updateItinerary i ((n,d):nds) (via,dist) nc = updateItinerary adjusted nds (via,dist) nc
-    where
-    adjusted :: Itinerary
-    adjusted = Data.PSQueue.adjust (updateRoute (dist+d,nc) via) n i
+type Node = Int
+type Cost = (Length, NodeCount)
+type Length = Int
+type NodeCount = Int
+data Distance = Distance Cost (Maybe Node)
 
-routeMap :: [Edge] -> RouteMap
-routeMap = Data.Map.fromList 
-    . Prelude.map (\ps -> (fst (head ps),Prelude.map snd ps))
-    . groupBy (\a b -> fst a == fst b)
-    . sort
-    . Prelude.map nodeAndNeighbor
-    where
-    nodeAndNeighbor (n,m,d) = (n,(m,d))
+cost :: Distance -> Cost
+cost (Distance c _) = c
 
-neighbors :: Node -> RouteMap ->[Neighbor]
-neighbors = findWithDefault []
+via :: Distance -> Maybe Node
+via (Distance _ n) = n
 
-nodes :: RouteMap -> [Node]
-nodes r = nub $ sort $ (Data.Map.keys r) ++ Data.List.map fst (concat (Data.Map.elems r))
-
-routes :: RouteMap -> Node -> Itinerary
-routes r n = calcRoutes r Data.PSQueue.empty (itinerary (nodes r) n)
-
-calcRoutes :: RouteMap -> Itinerary -> Itinerary -> Itinerary
-calcRoutes m dest srce = case Data.PSQueue.minView srce of
-                    Nothing -> dest
-                    Just ((n :-> Route (dist,nc) v),srce') -> calcRoutes m dest' srce''
-                        where
-                        dest' = Data.PSQueue.insert n (Route (dist,nc) v) dest
-                        srce''= updateItinerary srce' (neighbors n m) (n,dist) (nc+1)
-                                
- 
-shortest :: RouteMap -> Node -> Node -> [(Node,Distance)]
-shortest m s e = reverse (pathTo (Just e) (routes m s))
-
-pathTo :: Maybe Node -> Itinerary -> [(Node,Distance)]
-pathTo Nothing _ = []
-pathTo (Just n) i = case Data.PSQueue.lookup n i of
-    Just (Route (d,c) v) -> (n,d) : pathTo v i
-
-solve :: [[Int]] -> (Distance,[Node])
-solve ([n,start,end]:edges) = let
-    m = routeMap (Data.List.map (\[a,b,c]->(a,b,c)) edges) 
-    p = shortest m start end
-    d = snd (last p)
-    ns= Data.List.map fst p
-    in (d,ns)
-solve ns = error $ "unexpected entry:" ++ (show ns)
-
-process :: [String] -> [String]
-process ss = let p = Data.List.map (Data.List.map read . words) ss 
-                 (d,ns) = solve p
-             in [show d, concat (intersperse " " (Data.List.map show ns))]
+infinity :: Distance
+infinity = Distance (maxBound, maxBound) Nothing
--- Specs.hs
+++ Specs.hs
@@ -3,129 +3,19 @@ import Dijkstra
 import Data.PSQueue
 
 main = hspec $ do
-    describe "a route" $ do
-        describe "when undefined" $ do
-            it "has an infinite distance" $ do
-                distance undefinedRoute `shouldBe` maxBound
-
-            it "has no via node" $ do
-                via undefinedRoute `shouldBe` Nothing
-
+    describe "a distance" $ do
         describe "when defined" $ do
-            let r = route (100,2) 4807
-            it "has a finite distance" $ do
-                distance r `shouldBe` 100
+            let d = Distance (100,1) (Just 0)
+            it "has a cost " $ do
+                cost d  `shouldBe` (100,1)  
 
             it "has a via node" $ do
-                via r `shouldBe` Just 4807
-
-        describe "when initial" $ do
-            let r = initialRoute
-            it "has a null distance" $ do
-                distance r `shouldBe` 0
+                via d  `shouldBe` (Just 0) 
+            
+        describe "when undefined" $ do
+            let d = infinity
+            it "has an infinite cost " $ do
+                cost d  `shouldBe` (maxBound,maxBound)
 
             it "has no via node" $ do
-                via r `shouldBe` Nothing
-
-        describe "can be updated" $ do
-            let r = undefinedRoute
-                r'= updateRoute (100,0) 4807 r
-            it "with a smaller distance" $ do
-                r' `shouldBe` Route (100,0) (Just 4807)
-            it "and with a smaller distance only" $ do
-                let r'' = updateRoute (150,0) 42 r'
-                r'' `shouldBe` r' 
-
-    describe "an itinerary" $ do
-        let i = itinerary [0..4] 3
-        it "can be initialized with a list of nodes and a starting node" $ do
-            toList i  `shouldBe` [0 :-> Route infinite Nothing
-                                 ,1 :-> Route infinite Nothing
-                                 ,2 :-> Route infinite Nothing
-                                 ,3 :-> Route (0,0) Nothing
-                                 ,4 :-> Route infinite Nothing] 
-
-        it "can be updated with a list of neighbors to a node" $ do
-            let i'= updateItinerary i [(0,200),(1,400)] (3,0) 1
-            toList i' `shouldBe` [0 :-> Route (200,1) (Just 3)
-                                 ,1 :-> Route (400,1) (Just 3)
-                                 ,2 :-> Route infinite Nothing
-                                 ,3 :-> Route (0,0) Nothing
-                                 ,4 :-> Route infinite Nothing] 
-            let i''= updateItinerary i' [(2,300)] (0,200) 1
-            toList i'' `shouldBe` [0 :-> Route (200,1) (Just 3) 
-                                 ,1 :-> Route (400,1) (Just 3)
-                                 ,2 :-> Route (500,1) (Just 0) 
-                                 ,3 :-> Route (0,0) Nothing
-                                 ,4 :-> Route infinite Nothing] 
-    describe "a routeMap" $ do
-        let m = routeMap [(0,1,300),(0,2,400),(0,3,200)
-                         ,(1,4,200),(1,2,400),(2,4,600)
-                         ,(3,4,100),(3,5,400),(4,5,200)]
-        it "can be searched for neighbors of a node" $ do
-            neighbors 0 m `shouldBe` [(1,300),(2,400),(3,200)] 
-
-        it "has a list of nodes" $ do
-            nodes m  `shouldBe` [0,1,2,3,4,5]
-
-    describe "routes" $ do
-        it "finds the routes in a route map from a node to all other nodes" $ do
-            let m = routeMap [(0,1,300),(0,2,400),(0,3,200)
-                             ,(1,4,200),(1,2,400),(2,4,600)
-                             ,(3,4,100),(3,5,400),(4,5,200)]
-
-            toList (routes m 0) `shouldBe`  [0 :-> Route (0,0) Nothing
-                                            ,1 :-> Route (300,1) (Just 0)
-                                            ,2 :-> Route (400,1) (Just 0)
-                                            ,3 :-> Route (200,1) (Just 0)
-                                            ,4 :-> Route (300,2) (Just 3)
-                                            ,5 :-> Route (500,3) (Just 4)]
-
-    describe "shortest" $ do
-        it "is the shortest path from a node to another in a route map" $ do
-            let m = routeMap [(0,1,300),(0,2,400),(0,3,200)
-                             ,(1,4,200),(1,2,400),(2,4,600)
-                             ,(3,4,100),(3,5,400),(4,5,200)]
-            shortest m 0 5 `shouldBe` [(0,0),(3,200),(4,300),(5,500)]
-
-        it "select the path with less nodes in case where several are possible" $ do
-            let m = routeMap [(0,1,100),(0,2,100),(1,3,050),(2,4,100),(3,4,50)]
-            toList (routes m 0) `shouldBe` [0 :-> Route (0,0) Nothing
-                                           ,1 :-> Route (100,1) (Just 0)
-                                           ,2 :-> Route (100,1) (Just 0)
-                                           ,3 :-> Route (150,2) (Just 1)
-                                           ,4 :-> Route (200,2) (Just 2)]
-            shortest m 0 4 `shouldBe` [(0,0),(2,100),(4,200)]
-            let m = routeMap [(0,1,100),(0,2,100),(1,4,100),(2,3,50),(3,4,50)]
-            toList (routes m 0) `shouldBe` [0 :-> Route (0,0) Nothing
-                                           ,1 :-> Route (100,1) (Just 0)
-                                           ,2 :-> Route (100,1) (Just 0)
-                                           ,3 :-> Route (150,2) (Just 2)
-                                           ,4 :-> Route (200,2) (Just 1)]
-            shortest m 0 4 `shouldBe` [(0,0),(1,100),(4,200)]
-
-    describe "solve" $ do
-        it "solve a shortpath problem from a list of integers" $ do
-            let ns = [[6,0,5],[0,1,300],[0,2,400],[0,3,200]
-                             ,[1,4,200],[1,2,400],[2,4,600]
-                             ,[3,4,100],[3,5,400],[4,5,200]]
-            solve ns `shouldBe` (500,[0,3,4,5])
-
-    describe "process" $ do
-        it "process a list of string to solve the problem" $ do
-            let ss = ["6 0 5","0 1 300","0 2 400","0 3 200"
-                             ,"1 4 200","1 2 400","2 4 600"
-                             ,"3 4 100","3 5 400","4 5 200"]
-            process ss  `shouldBe` ["500","0 3 4 5"]
-
-
-            
-
-
-            
-            
-    
-
-   
-            
-                
+                via d  `shouldBe` Nothing
